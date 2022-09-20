8abe91910f6ed35e527e288c2ac4af1e50221fb4
Elena <p0zhar@yandex.ru>
2022-09-20 16:42:43 +0400

    ИТОГОВАЯ Задача

---
+++ Final_Task01/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк 
+// формирует массив из строк, длина которых меньше либо равна 3 символам. 
+// Первоначальный массив ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+// Примеры:
+// ["hello", "2", "world", ";-)"] -> ["2", ";-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+Console.WriteLine("Программа из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символам.");
+Console.WriteLine();
+
+string[] array = new string[8] {"1234", "1567", "-2", "computer science", "hello", "2", "world", ";-)"};
+string[] arrayNew = new string[array.Length];
+
+void NewArray(string[] array, string[] arrayNew)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        arrayNew[count] = array[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}   ");
+    }
+    Console.WriteLine();
+}
+
+NewArray(array, arrayNew);
+Console.WriteLine("Изначальный массив:");
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine("Новый массив:");
+PrintArray(arrayNew);
+Console.WriteLine();
+
